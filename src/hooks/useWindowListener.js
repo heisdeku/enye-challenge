15e828982569a33a413a3ea60df5a07c8612bfd7
Oluwaferanmi Adedokun <adedokunoluwaferanmi@gmail.com>
2021-01-31 16:39:31 +0100

    feat: search functionality

--- src/hooks/useWindowListener.js
+++ src/hooks/useWindowListener.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 const useWindowListener = () => {
   const [width, setWidth] = useState(window.innerWidth)
@@ -9,7 +9,7 @@ const useWindowListener = () => {
   useEffect(() => {
     window.addEventListener('resize', setSize)
     return () => window.removeEventListener('resize', setSize)
-  }, [])
+  })
   return [width]
 }
 
